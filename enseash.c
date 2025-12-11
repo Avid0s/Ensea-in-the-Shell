2ea101634dc372cd7a78a204f93e224c5776fe1d
ensea <ensea@StudentLab>
2025-12-11 09:43:45 +0100

    premiere question enseash

---
+++ enseash.c
@@ -0,0 +1,26 @@
+//test
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main(void){
+	char *welcome = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+	char *prompt = "enseash % ";
+    int BUFSIZE = strlen(welcome); //On veut la taille de notre message pour utiliser write apres
+
+
+    write(1, welcome,BUFSIZE); // on utilise "1" pour l'afficher directement dans le terminal
+
+	while (1) {
+        write(1, prompt, strlen(prompt));
+        while(1){   // ici, on met un while(1) pour bloquer et prendre une capture d'écran.
+
+        }
+	}
+
+}
+
