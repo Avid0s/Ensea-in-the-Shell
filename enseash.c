0e85e5790baf14b397e0da8bcd4d9cc7881d3a8c
Avid0s <gabriel.vidon@ensea.fr>
2025-12-11 11:03:23 +0100

    Replace magic number with STDOUT_FILENO for clarity

--- enseash.c
+++ enseash.c
@@ -13,10 +13,10 @@ int main(void){
     int BUFSIZE = strlen(welcome); //On veut la taille de notre message pour utiliser write apres
 
 
-    write(1, welcome,BUFSIZE); // on utilise "1" pour l'afficher directement dans le terminal
+    write(STDOUT_FILENO, welcome,BUFSIZE); // on utilise "1" pour l'afficher directement dans le terminal
 
 	while (1) {
-        write(1, prompt, strlen(prompt));
+        write(STDOUT_FILENO, prompt, strlen(prompt));
         while(1){   // ici, on met un while(1) pour bloquer et prendre une capture d'écran.
 
         }
