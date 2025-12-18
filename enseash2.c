c27e62ef6b1d1c63c8b649c8b8cb89fd41591e34
Avid0s <gabriel.vidon@ensea.fr>
2025-12-18 09:37:25 +0100

    Update command execution method in enseash2.c
    Refactor command execution to use execvp with argv_exec array.

--- enseash2.c
+++ enseash2.c
@@ -10,21 +10,27 @@ int main(void){
 	char *prompt = "enseash % ";
 	char input[128];
 	int message_size;
+	char *argv_exec[2];
+    argv_exec[0] = input;   // commande tapée
+    argv_exec[1] = NULL;    
+
 
     write(STDOUT_FILENO, welcome,strlen(welcome)); // on utilise "STDOUT_FILENO" pour l'afficher directement dans le terminal
 
 	while (1) {
         write(STDOUT_FILENO, prompt, strlen(prompt));
         message_size= read(STDIN_FILENO,input, sizeof(input)-1);  // on utilise "STDIN_FILENO" pour lire l'entrée
-        input[message_size-1] = '\0';  							  // pour definir la fin du message et remplacer le \n
+        input[message_size-1] = '\0';  // pour definir la fin du message, et on enleve \n
+		
+        if ((strcmp(input ,"exit\0") ==0)  || ((strcmp(input ,"\0") ==0) )  ){ // on fait une comparaison des mots du message en éntrée et 'exit', et on break si c'est exit.
+        													// le CTRL+D est équivalent à unmessage vide --> \0 donc on utilise cette deuxiemme comparaison
+        	write(STDOUT_FILENO, ciao, strlen(ciao));
+        	break;
+		}
         
-       
-        if (strcmp(input, "fortune\0") == 0) { // on fait une comparaison des mots du message en éntrée et 'exit', et on break si c'est exit.
-											   // on n'oublie pas le \n\0 car quand on appuie sur la touche 'entrée' cela fait \n									
-            pid_t pid = fork();
-            if (pid == 0){
-                        execlp("fortune", "fortune", NULL);
-                        
+        pid_t pid = fork();
+        if (pid == 0){
+            execvp(argv_exec[0], argv_exec);
             } else {
                     // parent attend la fin de l'enfant, car sinon on ne voit plus le prompt réapparaitre
                     wait(NULL);
